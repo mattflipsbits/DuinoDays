b84308e645477932f8b115f27f0bde936d3b109b
mattflipsbits <mattflipsbits@gmail.com>
2026-03-07 23:54:44 -0700

    Added Blink_empty

---
+++ Day2/Blink_empty/Blink_empty.ino
@@ -0,0 +1,20 @@
+// Create an int variable named ledPin and assign the value 13 to it.
+
+// Create an int variable named delayTime and assign the value 500 to it.
+
+void setup() {
+  // Use the pinMode() function to set the on-board LED pin (stored in ledPin)
+  // to OUTPUT. Use the ledPin variable you created above.
+
+  
+}
+
+void loop() {
+  // Set the on-board LED to HIGH using digitalWrite() and the ledPin variable.
+
+  // Wait 500 milliseconds using the delayTime variable.
+
+  // Set the on-board LED to LOW using digitalWrite() and the ledPin variable.
+
+  // Wait 500 milliseconds using the delayTime variable.
+}
